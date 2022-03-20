899cb045fd01124b9036a475f7db03a701f59e8d
Chanti <chvenkatesh.1118@gmail.com>
2022-03-20 18:55:07 +0530

    20-03

--- tomcat.sh
+++ tomcat.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install java -y
 wget -O /tmp/tomcat.gz https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.60/bin/apache-tomcat-9.0.60.tar.gz --no-check-certificate
 tar -xvzf /tmp/tomcat.gz
-cd tmp
+cd /tmp
 mv apache-tomcat-9.0.60 tomcat
 
 cd tomcat/bin
