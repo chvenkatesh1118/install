ab9d9e2ac94658203f1b5be7333b915fb50bb977
Chanti <chvenkatesh.1118@gmail.com>
2022-03-24 13:00:31 +0530

    24-03

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,20 +1,38 @@
-CNI_VERSION="v0.8.2"
-ARCH="amd64"
-sudo mkdir -p /opt/cni/bin
-curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-linux-${ARCH}-${CNI_VERSION}.tgz" | sudo tar -C /opt/cni/bin -xz
-DOWNLOAD_DIR=/usr/local/bin
-sudo mkdir -p $DOWNLOAD_DIR
-CRICTL_VERSION="v1.22.0"
-ARCH="amd64"
-curl -L "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" | sudo tar -C $DOWNLOAD_DIR -xz
-RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
-ARCH="amd64"
-cd $DOWNLOAD_DIR
-sudo curl -L --remote-name-all https://storage.googleapis.com/kubernetes-release/release/${RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet,kubectl}
-sudo chmod +x {kubeadm,kubelet,kubectl}
-
-RELEASE_VERSION="v0.4.0"
-curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | sudo tee /etc/systemd/system/kubelet.service
-sudo mkdir -p /etc/systemd/system/kubelet.service.d
-curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | sudo tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-systemctl enable --now kubelet
+#Download the latest release with the command:
+#
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#Note:
+#To download a specific version, replace the $(curl -L -s https://dl.k8s.io/release/stable.txt) portion of the command with the specific version.
+#
+#For example, to download version v1.23.0 on Linux, type:
+#
+#curl -LO https://dl.k8s.io/release/v1.23.0/bin/linux/amd64/kubectl
+#Validate the binary (optional)
+#
+#Download the kubectl checksum file:
+#
+#curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+#Validate the kubectl binary against the checksum file:
+#
+#echo "$(<kubectl.sha256)  kubectl" | sha256sum --check
+#If valid, the output is:
+#
+#kubectl: OK
+#If the check fails, sha256 exits with nonzero status and prints output similar to:
+#
+#kubectl: FAILED
+#sha256sum: WARNING: 1 computed checksum did NOT match
+#Note: Download the same version of the binary and checksum.
+#Install kubectl
+#
+#sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#Note:
+#If you do not have root access on the target system, you can still install kubectl to the ~/.local/bin directory:
+#
+#chmod +x kubectl
+#mkdir -p ~/.local/bin
+#mv ./kubectl ~/.local/bin/kubectl
+## and then append (or prepend) ~/.local/bin to $PATH
+#Test to ensure the version you installed is up-to-date:
+#
+#kubectl version --client
