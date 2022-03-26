9c0795392abb4d65da3fcebc466a42e8006442f5
Chanti <chvenkatesh.1118@gmail.com>
2022-03-26 10:58:50 +0530

    24-03

---
+++ infra/instance.tf
@@ -0,0 +1,18 @@
+variable "COUNT" {}
+variable "NAME" {}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "EC2" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t2.micro"
+  security_groups = ["sg-03439c9546a230549"]
+  subnet_id = "subnet-0f42b250c3cf1d75c"
+  count = var.COUNT
+
+  tags = {
+    Name = var.NAME
+  }
+}
