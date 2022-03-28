ae0fb703a915022b1b1655a4a853b35a721ec8fc
Chanti <chvenkatesh.1118@gmail.com>
2022-03-28 13:07:00 +0530

    24-03

--- kubemaster.sh
+++ kubemaster.sh
@@ -1,32 +1,23 @@
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo swapoff -a
+##docker installation
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt install docker.io
 
-yum install -y -q yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y -q docker-ce
-systemctl enable docker
-systemctl start docker
+sudo apt-get install -y kubelet kubeadm kubectl
+#sudo apt-mark hold kubelet kubeadm kubectl
+## above command hold for kubernetes when type get upate
+sudo kubeadm init --pod-network-cidr 192.168.0.0/16
+# ip range from 10.0.0.0   - 10.255.255.255
+#               172.16.0.0 - 172.31.25.255
+#               192.168.0.0 -
 
-setenforce 0
-sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
-systemctl disable firewalld
-systemctl stop firewalld
-sed -i '/swap/d' /etc/fstab
-swapoff -a
-cat >> /etc/sysctl.d/kubernetes.conf <<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sysctl --system
-cat >>/etc/yum.repos.d/kubernetes.repo<<EOF
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
 
-yum install -y kubeadm kubelet kubectl
-systemctl enable kubelet
-systemctl start kubelet
+
--- kubenode.sh
+++ kubenode.sh
@@ -28,4 +28,14 @@ EOF
 
 yum install -y kubeadm kubelet kubectl
 systemctl enable kubelet
-systemctl start kubelet
+
+
+cat >>/etc/yum.repos.d/kubernetes.repo<<EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
