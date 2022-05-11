81870a560e122b3c13c79248b949ce10b44fa6ef
Chanti <chvenkatesh.1118@gmail.com>
2022-05-11 14:34:45 +0530

    24-03

--- tomcat.sh
+++ tomcat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum update
+sudo yum update -y
 sudo yum install java -y
 wget -O /tmp/tomcat.gz https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.60/bin/apache-tomcat-9.0.60.tar.gz --no-check-certificate
 tar -xvzf /tmp/tomcat.gz
