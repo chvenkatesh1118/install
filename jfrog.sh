079aed7174ade5f072d906294856914b00d238cd
Chanti <chvenkatesh.1118@gmail.com>
2022-07-22 22:39:57 +0530

    24-03

--- jfrog.sh
+++ jfrog.sh
@@ -8,3 +8,13 @@ unzip jfrog-artifactory-oss-6.9.6.zip
 cd artifactory-oss-6.9.6
 cd bin
 bash artifactory.sh
+
+
+##########################################
+ubuntu
+######################################
+# To determine your distribution, run lsb_release -c or cat /etc/os-release
+# Example:echo "deb https://releases.jfrog.io/artifactory/artifactory-pro-debs xenial main" | sudo tee -a /etc/apt/sources.list;
+wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | sudo apt-key add -;
+echo "deb https://releases.jfrog.io/artifactory/artifactory-debs {distribution} main" | sudo tee -a /etc/apt/sources.list;
+sudo apt-get update && sudo apt-get install jfrog-artifactory-oss
